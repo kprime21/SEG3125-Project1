eb50017653489711829842d2e21aab88a8016934
kprime21 <kohuladaskirjan@gmail.com>
2022-06-04 16:15:01 -0400

    changed classname

--- components/Services.js
+++ components/Services.js
@@ -12,16 +12,16 @@ export default() => {
     return(
         <>
         <Services>
-        <div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
 </div>
-<div class="card" >
+<div className="card" >
   
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
         </Services>
