1b6a82d8ab6fcb3d4563716156059943db13e3a1
kprime21 <kohuladaskirjan@gmail.com>
2022-06-06 08:23:15 -0400

    changed class -> className

--- components/Hero.js
+++ components/Hero.js
@@ -108,11 +108,11 @@ export default function Hero() {
     
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
   <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
 </CarouselHolder>
